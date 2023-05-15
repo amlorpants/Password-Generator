2abb51dc2f38f5e378626e6c0c81e04dab1a070c
amtay <tayloramydesign@gmail.com>
2023-05-15 14:03:57 -0500

    adds character length js function

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 
 
 // Get references to the #generate element
-var generateBtn = document.querySelector("#generate");
+
 
 // Write password to the #password input
 function writePassword() {
@@ -13,5 +13,25 @@ function writePassword() {
 
 }
 
+function generatePassword() {
+  var passwordOptions = createPasswordOptions();
+}
+
+function createPasswordOptions() {
+var passwordLength = prompt(
+  'How long would you like your password to be? (Must be 10 - 128 characters in length)'
+);
+
+if (passwordLength < 10 || passwordLength > 128) {
+alert(
+'please try again, password length must be 10 - 128 characters in length'
+);
+return null;
+}
+
+}
+
+// Get references to the #generate element
+var generateBtn = document.querySelector("#generate");
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
